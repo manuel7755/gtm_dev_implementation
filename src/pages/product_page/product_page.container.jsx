acdc4977453c40020b319712215ae8957e9f3d74
MJ3132 <martin_jarrin@hotmail.com>
2022-01-05 15:46:09 -0500

    dataLayer

--- src/pages/product_page/product_page.container.jsx
+++ src/pages/product_page/product_page.container.jsx
@@ -2,42 +2,46 @@ import React, { Component, useState, useContext, useEffect } from 'react';
 import './product_page.styles.scss';
 
 import { apiGetProduct, getQueryParam, addToCart, getCartInfo } from '../../utility/Utility.component';
+
 import { Alert } from '@material-ui/lab';
 import { CartContext } from '../../Contexts/Cart';
+
 import ReviewStars from "../../components/ReviewStars/ReviewStars.component";
-
-
 import Button from '../../components/Button/Button.component';
 
-const ProductPage = () => {
+import TagManager from "react-gtm-module";
 
+const ProductPage = () => {
 
     const [product, setProduct] = useState({})
     const [successAddToCart, setSuccessAddToCart] = useState(false);
     const { cart, setCart } = useContext(CartContext);
-
-
 
     useEffect(() => {
         const pCode = getQueryParam('pCode');
 
         apiGetProduct(pCode, (product) => {
 
-            console.log(product)
+            TagManager.dataLayer({
+                dataLayer: {
+                    event: "pageview",
+                    page: {
+                        path: "/product?pCode=" + product.id,
+                        pageType: "productPage"
+                    },
+
+                    products: [{ ...product }]
+                }
+            })
 
             setProduct({ ...product, quantity: 1 })
 
         })
-
-        //get product quantity
     }, [])
 
     function updateProductQuantity(e) {
 
         let updatedProduct = { ...product, quantity: +e.target.value };
-
-        console.log('updated quantity', updatedProduct)
-
 
         setProduct(updatedProduct)
     }
@@ -47,20 +51,12 @@ const ProductPage = () => {
 
         let updatedProduct = { ...product };
 
-        console.log('product at addToCart ', updatedProduct)
-
 
         addToCart(cart, updatedProduct).then((updatedCart) => {
 
-            console.log('product has been added to cart', updatedCart);
-
             setSuccessAddToCart(true)
 
-
             setCart({ ...updatedCart })
-
-            console.log('cartInfo ', updatedCart.cartInfo)
-
 
         })
         setTimeout(() => {
@@ -70,62 +66,40 @@ const ProductPage = () => {
         }, 3500)
     }
 
-
     return (
-
         <div className='page_section productPage_container'>
             <div className='productPage_container_product_container'>
                 <div className='product_image_container'>
                     <img src={product.image} alt='' className='product_image' />
                     <div className='product_reviews'>
-
                         <h3>
-                           <ReviewStars rate={product && product.rating && product.rating.rate ? product.rating.rate : null}/>
-
+                            <ReviewStars rate={product && product.rating && product.rating.rate ? product.rating.rate : null} />
                         </h3>
-
                         <h3>
                             Count: {product && product.rating && product.rating.count ? product.rating.count : null}
                         </h3>
-
                     </div>
-
                 </div>
-
                 <div className='product_information'>
                     <div className='product_name'>
-
                         <h1> {product.title} </h1>
-
                     </div>
-
                     <div className="product_description">
                         <p> {product.description}</p>
-
                     </div>
-
                     <div className='product_quantity'>
                         <div className="quantity_label">
                             <h2>QTY</h2>
-
-
                         </div>
                         <div className="quantity_container">
                             <input onChange={(e) => updateProductQuantity(e)} type='number' defaultValue='1' name='quantity' min='1' />
                         </div>
-
-
                     </div>
-
                     <div className='addTocartButton'>
-
                         <Button link={window.location.pathname + window.location.search} clickAction={() => nativeAddToCart()}>Add To Cart</Button>
-
                     </div>
                     <br />
-
                     {successAddToCart ?
-
                         <Alert variant="filled" severity="success">
                             Success You Have Added To Cart!
                         </Alert>
