ccbd3bb5e816177a999ed6ee75e9b22a263d62d0
MJ3132 <martin_jarrin@hotmail.com>
2022-01-09 16:34:56 -0500

    changed products metric to product object for easier dataLayer accesbility

--- src/pages/product_page/product_page.container.jsx
+++ src/pages/product_page/product_page.container.jsx
@@ -29,7 +29,7 @@ const ProductPage = () => {
                         pageType: "productPage"
                     },
 
-                    products: [{ ...product }]
+                    product: [{ ...product }]
                 }
             })
 
